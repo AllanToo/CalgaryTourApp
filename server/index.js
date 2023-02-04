8c2e686af656eea8fd221f0664f58b0170ba595d
Jaeho Kim <101899896+jaehokimdev@users.noreply.github.com>
2023-02-04 10:24:25 -0700

    working now2

--- server/index.js
+++ server/index.js
@@ -4,6 +4,25 @@ const cors = require("cors");
 const port = process.env.PORT || 3001;
 const { mongodbaddress } = "localhost";
 
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://root:passwordpassword@cluster1.r6doziu.mongodb.net/?retryWrites=true&w=majority";
+
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connect to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+}
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect((err) => {
+  const db = client.db("test");
+  console.log("Connected to MongoDB successfully");
+  client.close();
+});
+
 app.use(cors());
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Main } from "./pages/Main";
 import { Layout } from "./component/Layout";
+import { Hosts } from "./pages/Hosts";
 
 function App() {
   return (
@@ -23,6 +24,14 @@ function App() {
             element={
               <Layout>
                 <Main />
+              </Layout>
+            }
+          />
+          <Route
+            path="hosts"
+            element={
+              <Layout>
+                <Hosts />
               </Layout>
             }
           />
