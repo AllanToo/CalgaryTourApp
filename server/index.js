6dd2311c4e22b983ef99136523f097c394504930
Allan To <113399719+AllanToo@users.noreply.github.com>
2023-07-05 21:58:45 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-require("dotenv/config");
+
 const bodyParser = require("body-parser");
 
 app.use(cors());
